e76f3254c9bc81da98bf404f14db070c4875ed83
Fafaew <rafael.gomest@gmail.com>
2020-04-16 22:13:17 -0300

    Initializing database connection

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,5 @@
 module.exports = {
-    age: function(timestamp) {
+    age(timestamp) {
             const today = new Date()
             const birthDate = new Date(timestamp)
 
@@ -15,7 +15,7 @@ module.exports = {
         
             return age
         },
-        date: function(timestamp){
+        date(timestamp){
             const date = new Date(timestamp)
 
             const year = date.getUTCFullYear()
@@ -27,7 +27,9 @@ module.exports = {
                 month,
                 year,
                 iso: `${year}-${month}-${day}`,
-                birthDay: `${day}/${month}`
+                birthDay: `${day}/${month}`,
+                format: `${day}/${month}/${year}`
+                
             }
         }
     
